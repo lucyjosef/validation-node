d33f39f4ccf28a5fa488e1e0773b369ebb9f6c67
lucyjosef <luciejoseph@ymail.com>
2018-11-15 13:02:02 +0100

    connected front with secret's POST request

--- public/client.js
+++ public/client.js
@@ -2,7 +2,30 @@
 	const vm = new Vue({
 		el: '#app',
 		data:{
-			title: 'Validation Node2'
+			title: 'Validation Node2',
+			secretvalue: '',
+		},
+		methods: {
+			editSecret: function() {
+				data = {"text": document.getElementById('editSecret').value}
+				this.secretvalue = data.text
+				console.log(data.text)
+				fetch('http://localhost:4001/secret', {
+					method: 'POST',
+					headers: {
+						'Content-type': 'application/json',
+						'Access-Control-Allow-Origin': '*'
+					},
+					body: JSON.stringify(data)
+				})
+					.then((res) => {
+						console.log(res)
+						return res.json()
+					})
+					.catch((err) => {
+						console.log(err)
+					})
+			}
 		}
 	})
 }
--- secret-server.js
+++ secret-server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const fs = require('fs')
 const fse = require('fs-extra')
+const cors = require('cors')
 
 const port = 4001
 
@@ -8,6 +9,9 @@ const app = express()
 
 const jsonMiddleware = express.json()
 app.use(jsonMiddleware)
+
+app.use(cors())
+app.options('*', cors())
 
 
 /* secret server */
@@ -23,7 +27,7 @@ app.get('/secret', function(req, res){
 	})
 })
 
-app.post('/secret', function(req,res) {
+app.post('/secret', (req,res) => {
 	const reversed = reverseString(req.body.text.toString())
 	fs.writeFile(__dirname + '/data/secret.txt', reversed, 'utf8', ()=>{
 		res.json(reversed)
