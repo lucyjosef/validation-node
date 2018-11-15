2d59bdd6e90f986aa5908fe5c0ce6046dca46c42
lucyjosef <luciejoseph@ymail.com>
2018-11-15 11:49:19 +0100

    implemented static http server on port 8000

--- index.js
+++ index.js
@@ -1,36 +1,26 @@
 const express = require('express')
 const fs = require('fs')
 
-const port1 = 4000
-const port2 = 4001
+const port = 8000
 const port3 = 4002
 const port4 = 4003
 
-const app1 = express()
-const app2 = express()
+const app = express()
 const app3 = express()
 const app4 = express()
 
-
-/* APP 1 */
-app1.listen(port1, () => {
-  console.log(`Listening on ${port1}`)
+app.listen(port, () => {
+	console.log(`Listening on ${port}`)
 })
 
-app1.get('/', function(req, res){
-	console.log('route de base')
+app.use(
+  '/client',
+  express.static(__dirname + '/public')
+)
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html')
 })
-
-
-/* APP 2 */
-app2.listen(port2, () => {
-  console.log(`Listening on ${port2}`)
-})
-
-app2.get('/secret', function(req, res){
-	console.log('secret')
-})
-
 
 /* APP 3 */
 app3.listen(port3, () => {
