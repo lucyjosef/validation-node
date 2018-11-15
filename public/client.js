a623f6434058e5180f572beed614cf2688128d77
Jeremie Soffichiti <jeremie.soffichiti@gmail.com>
2018-11-15 15:36:58 +0100

    Add setInterval with fetch time-server

--- public/client.js
+++ public/client.js
@@ -1,8 +1,62 @@
 {
 	const vm = new Vue({
-		el: '#app',
+        el: '#app',
+        created:function(){
+
+        setInterval(()=>{
+            fetch('http://localhost:4000/',
+            {  
+                headers: {
+                'Accept': 'application/json'
+                },
+            })
+            .then((res) =>{
+                return res.json()
+            })
+            .then((myJson) =>{
+                let data = JSON.parse(myJson)
+
+                if(this.timeServ.length === 10){
+                    this.timeServ.shift()
+
+                    data.forEach(el =>{ 
+                        this.timeServ.push({'date': el.date, "time": Date.now()})
+                    })
+
+                }else{
+
+                    data.forEach(el =>{ 
+                        // this.timeServ.push(el)
+                        this.timeServ.push({'date': el.date, "time": Date.now()})
+
+                    })
+                }
+            })
+
+            .catch(err =>{
+                console.log(err)
+                this.timeServ.push({'date': 'DOWN'})
+            })
+        }, 1000)
+
+
+            
+          
+          },
 		data:{
-			title: 'Validation Node2'
-		}
+            title: 'Validation Node2',
+            timeServ:[],
+        }, 
+        methods:{
+            dateDiff : (arg) => {
+                let timeNow = Date.now()
+                console.log(timeNow, arg)
+
+                let diff = timeNow - arg
+                let result = Math.floor(diff / 1000);
+              return result
+            }
+            
+        }
 	})
 }
