3b4ba7913fc75fe7fe5390df373fc3a925d79f76
Maxime Delaporte <delaporte.maxime@orange.fr>
2018-11-15 17:20:18 +0100

    Url parameters errors

--- sechisrv.js
+++ sechisrv.js
@@ -20,15 +20,31 @@ app.listen(port, () => {
 })
 app.get('/:id', (req, res) =>{
   const id = req.params.id
-    console.log(id)
-  res.send(log[id - 1 ])
+  if (typeof log[id - 1 ] != "undefined"){
+    res.statusCode = 200
+    res.send(log[id - 1 ])
+  }
+  else if (Number(id) != 'NaN') {
+    if(id < 10 || id > 1){
+      res.statusCode = 404
+      res.send('Cet index n\'existe pas')
+    }
+    else{
+      res.statusCode = 400
+      res.send('Le parametre n\'est pas correct')
+    }
+  }
+  else{
+    res.statusCode = 400
+    res.send('Le parametre n\'est pas correct')
+  }
 })
 app.get('/', (req, res) => {
   const combinedData = {"time":{},"secret":{}}
   Promise.all([
-  fetch('http://localhost:4000/',options_date).then(function(response){ return response.json()} ),
-  fetch('http://localhost:4001/secret').then(function(response){return response.text()})
-])
+    fetch('http://localhost:4000/',options_date).then(function(response){ return response.json()} ),
+    fetch('http://localhost:4001/secret').then(function(response){return response.text()})
+  ])
   .then((values)=>{
     combinedData["time"] = values[0]
     const dateJson = JSON.parse(combinedData["time"])
