6f47899e9d4dd8af27762a95b245b77c89b0faf9
admin <admin@iMac3-7.local>
2018-11-15 11:30:31 +0100

    Start Fetching server time and secret

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const fs = require('fs')
 
 const port1 = 4000
 const port2 = 4001
-const port3 = 4002
 const port4 = 4003
 
 const app1 = express()
 const app2 = express()
-const app3 = express()
 const app4 = express()
 
 
@@ -29,12 +27,6 @@ app2.listen(port2, () => {
 
 app2.get('/secret', function(req, res){
 	console.log('secret')
-})
-
-
-/* APP 3 */
-app3.listen(port3, () => {
-  console.log(`Listening on ${port3}`)
 })
 
 /* APP 4 */
