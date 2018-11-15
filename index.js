0e3fa5ab8428a04ef7c568732b38d5d554a79d10
lucyjosef <luciejoseph@ymail.com>
2018-11-15 12:04:02 +0100

    installed vue lib locally

--- index.js
+++ index.js
@@ -3,12 +3,8 @@ const fs = require('fs')
 fse = require('fs-extra')
 
 const port = 8000
-const port3 = 4002
-const port4 = 4003
 
 const app = express()
-const app3 = express()
-const app4 = express()
 
 app.listen(port, () => {
 	console.log(`Listening on ${port}`)
@@ -22,17 +18,3 @@ app.use(
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html')
 })
-
-/* APP 3 */
-app3.listen(port3, () => {
-  console.log(`Listening on ${port3}`)
-})
-
-/* APP 4 */
-app4.listen(port4, () => {
-	console.log(`Listening on ${port4}`)
-})
-app4.use(
-  '/client',
-  express.static(__dirname + '/public')
-)
